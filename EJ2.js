b19cec45ee24dc1dc75c58ca46acd58b4547959d
MonseHernandez4 <monserrat.hernandezen@uanl.edu.mx>
2022-10-12 11:01:51 -0500

    Avance

---
+++ EJ2.js
@@ -0,0 +1,7 @@
+/* Escribir un programa en JavaScript que permita:
+Imprimir en pantalla n numeros perfectos.
+El usuario podrá indicar cuantos numeros desea ver.
+Un numero perfecto es aquel que la suma de sus divisores excepto el mismo, da como
+resultado el mismo numero.
+Ejemplo: para el 6 sus divisores son 1,2,3,6
+Sumando todos excepto el 6: 1+2+3=6, 6 es perfecto*/
