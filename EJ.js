74ac5d8e0c381ef01c43d5e4938e56fb6546438e
MonseHernandez4 <monserrat.hernandezen@uanl.edu.mx>
2022-10-13 23:17:32 -0500

    Termine la act

--- EJ.js
+++ EJ.js
@@ -7,8 +7,13 @@ let frecuencia = 2;
 
 console.log("Hora incial de la toma: ", hora_ini);
 console.log("Frecuencia: ", frecuencia);
-console.log("Horarios")
 let num_tomas = (24/frecuencia);
-console.log("El paciente debe tomar al dia:", num_tomas);
+console.log("El paciente debe tomar:" , num_tomas, "tomas en total")
+console.log("Horarios");
 
-
+let horario = hora_ini;
+for (let i = 1;i <= num_tomas; i++) {
+    console.log("Toma " + i + " - " + (horario) + ":00");
+    horario = horario + frecuencia;
+    if (horario >= 24) {horario = horario - 24;};
+};
