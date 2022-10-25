0f3fef75f63cc7e2ac562964a847ec43b097c827
MonseHernandez4 <monserrat.hernandezen@uanl.edu.mx>
2022-10-25 17:17:44 -0500

    Puse la tabla de multiplicar

--- js/js5.js
+++ js/js5.js
@@ -1,3 +1,5 @@
+//Programa Calculadora
+
 function calcular(){
     let numero1 = parseInt(document.getElementById("numero1").value);
     let numero2 = parseInt(document.getElementById("numero2").value);
@@ -26,3 +28,39 @@ function calcular(){
             break;
     }
 }
+
+//Programa Tabla de Multiplicar
+
+function verTabla(){
+    let numerotabla = parseInt(document.getElementById("numerotabla").value);
+    let tipo = parseInt(document.getElementById("tipo").value);
+    let longi = parseInt(document.getElementById("longi").value); 
+    let resultado2 = document.getElementById("resultado2");
+    let push = document.createDocumentFragment();
+    
+    switch (tipo) 
+    {
+        case 1:
+            for (let i = 0 ; i <= longi; i++) 
+            {
+               let renglon = document.createElement("tr")
+               renglon.innerHTML = `<td>${numerotabla}</td><td>x</td><td>${i}</td><td>=</td><td>${(numerotabla * i)}</td>`;
+               push.appendChild(renglon);
+            };
+            resultado2.innerHTML = "";
+            resultado2.appendChild(push);
+            break;
+
+        case 2:
+            for (let i = longi; i >= 0 ; i--) 
+            {
+                let renglon = document.createElement("tr")
+                renglon.innerHTML = `<td>${numerotabla}</td><td>x</td><td>${i}</td><td>=</td><td>${(numerotabla * i)}</td>`;
+                push.appendChild(renglon);
+            };
+            resultado2.innerHTML = "";
+            resultado2.appendChild(push);
+            break;
+    };  
+    }
+
